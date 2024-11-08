fda9006b624dc6e6d918189b6b3c0029d0be087a
Maheen <mm3dc@virginia.edu>
2024-11-07 19:19:04 -0500

    Second run

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='booklover',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Maheen Mubashar',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages()
+)
